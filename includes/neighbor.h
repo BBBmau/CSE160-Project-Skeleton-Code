82e8c74a29a0141a40f99eb359967c07f58957ec
BBBmau <alvarez.mauriciotm@gmail.com>
2022-02-05 15:45:52 -0800

    Add Header file for neighbor data type

---
+++ includes/neighbor.h
@@ -0,0 +1,7 @@
+#ifndef NEIGHBOR_H
+#define NEIGHBOR_H
+
+typedef nx_struct neighbor{
+    nx_uint16_t home; // Node that we're focused on to find neighbors for
+    // Just one variable for now to have NeighborDiscovery working
+}
